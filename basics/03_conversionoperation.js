65835bf39d74105b012642a4ef78c2424ffd97b2
Durgesh kumar shukla <154465849+Durgesh991835@users.noreply.github.com>
2025-03-27 11:54:18 +0000

    comparison

--- basics/03_conversionoperation.js
+++ basics/03_conversionoperation.js
@@ -1,6 +1,6 @@
  let score = true
- console.log(typeof score);
- console.log(typeof (score));
+ //console.log(typeof score);
+// console.log(typeof (score));
 
  let valueInNumber = Number(score)
  //console.log(typeof valueInNumber);
@@ -21,4 +21,32 @@
 
  let someNumber = 33
  let stringNumber = String(someNumber)
- console.log(typeof stringNumber);
+ //console.log(typeof stringNumber);
+
+
+
+ //***************************  Operations */
+
+ let value = 3
+ let negvalue = -value
+ //console.log(negvalue);
+
+ //console.log(2+2);
+ //console.log(2-2);
+ //console.log(2*2);
+ //console.log(2/2);
+ //console.log(2%2);
+ //console.log(2**2);
+
+ let str1 = "Hello"
+ let str2 = " Durgesh"
+ let str3 = str1 + str2
+
+//  console.log(str3);
+
+//  console.log(1 + 2);
+//  console.log("1" + 2);
+//  console.log("1" + 2 + 2);
+//  console.log(1 + 2 + "2");
+
+
---
+++ basics/04_comparison.js
@@ -0,0 +1,18 @@
+// console.log(2 > 1);
+// console.log(2 >= 1);
+// console.log(2 <1 );
+// console.log(2 == 1);
+// console.log(2 != 1);
+
+console.log("2" > 1);
+console.log("02" > 1);
+
+// here string "2" automatic convert into number then result give true
+
+console.log(null > 0);
+console.log(null == 0);
+console.log(null >= 0);
+
+console.log(undefined > 0);
+console.log(undefined == 0);
+console.log(undefined >= 0);
